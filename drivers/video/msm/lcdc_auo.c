68355b1fa3fc8d9db977f180dc583f9b0370c17e
lupohirp <pakilodi@gmail.com>
2012-10-02 16:49:35 +0200

    solution to .35 lcd density size problems

--- drivers/video/msm/lcdc_auo.c
+++ drivers/video/msm/lcdc_auo.c
@@ -570,15 +570,15 @@ static int __init lcdc_auo_init(void)
 
 	pinfo.lcdc.h_back_porch = 12;
 	pinfo.lcdc.h_front_porch = 16;
-	pinfo.lcdc.h_pulse_width = 4;
-	pinfo.lcdc.v_back_porch = 3;
+	pinfo.lcdc.h_pulse_width = 40;
+	pinfo.lcdc.v_back_porch = 4;
 	pinfo.lcdc.v_front_porch = 3;
-	pinfo.lcdc.v_pulse_width = 2;
-	pinfo.lcdc.border_clr = 0;		/* blk */
+	pinfo.lcdc.v_pulse_width = 40;
+	pinfo.lcdc.border_clr = 0;	/* blk */
 	pinfo.lcdc.underflow_clr = 0xff;	/* blue */
 	pinfo.lcdc.hsync_skew = 0;
 	
-	ret = lcdc_device_register(&pinfo, &lcdc_auo_panel_data);
+	ret = lcdc_device_register(&pinfo);
 	if (ret)
 		printk(KERN_ERR "%s: failed to register device!\n", __func__);
 
